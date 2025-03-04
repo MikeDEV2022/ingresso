de8373003982d2afcce976eb20f621d2b5608f01
SrRaio <130098498+MikeDEV2022@users.noreply.github.com>
2025-03-04 18:01:03 -0300

    Adicionada toda a logica de programação do projeto

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// Declaração de variaveis globais
+
+let pista = 100;
+let superior = 200;
+let inferior = 400;
+
+function comprar(){
+    // Declaração local de variaveis
+
+    let select = document.getElementById("tipo-ingresso");
+    let qtd = document.getElementById("qtd").value;
+    let meuSelected = select.options[select.selectedIndex].value;
+
+    // Estrutura condicional
+    if(qtd == 0 || qtd == null){
+        alert("Digite a quantidade de ingresos que deseja comprar!")
+    }else{
+        if(meuSelected == "pista"){
+            pista = pista - qtd;
+            document.getElementById("qtd-pista").innerHTML = pista
+        }else if(meuSelected == "superior"){
+            superior = superior - qtd;
+            document.getElementById("qtd-superior").innerHTML = superior
+        }else if(meuSelected == "inferior"){
+            inferior = inferior - qtd;
+            document.getElementById("qtd-inferior").innerHTML = inferior
+        }
+    }
+    verificaIngreso(pista,superior,inferior,meuSelected);
+}
+
+function verificaIngreso(pista,superior,inferior,meuSelected){
+    if(meuSelected == "pista" && pista < 1){
+        alert("Acabou os ingresos em pista");
+        document.getElementById("qtd-pista").innerHTML = "0"
+    }else if(meuSelected == "superior" && superior < 1){
+        alert("Acabou os ingresos em superior");
+        document.getElementById("qtd-superior").innerHTML = "0"
+    }else if(meuSelected == "inferior" && inferior < 1){
+        alert("Acabou os ingresos em inferior");
+        document.getElementById("qtd-inferior").innerHTML = "0"
+    }
+} 
